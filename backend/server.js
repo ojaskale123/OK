1ae77cb759d756a07408518b50f98571718cada1
ojas kale <ojask68@gmail.com>
2026-04-22 15:18:42 +0530

    Move CORS to top of middleware stack to prevent masked errors

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ dotenv.config();
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
 
 // Basic Route for testing
 app.get('/', (req, res) => {
