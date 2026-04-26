1d27f953e99547555f7759ee3db100e04a12276c
Ojas Kale <ojask68@gmail.com>
2026-04-27 03:07:13 +0530

    fix: render port binding timeout

--- backend/server.js
+++ backend/server.js
@@ -49,5 +49,5 @@ app.use('/api/dashboard', dashboardRoutes);
 const attendanceRoutes = require('./src/routes/attendanceRoutes');
 app.use('/api/attendance', attendanceRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
